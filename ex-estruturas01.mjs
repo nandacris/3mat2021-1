b18b92f9c3f89d0422f7c2d9dc324f1ea0e27a4d
nandacris <nanda.brasilino75@gmail.com>
2021-05-12 15:08:07 +0000

    (12/05) Enunciados dos exercícios sobre estruturas de dados

---
+++ ex-estruturas01.mjs
@@ -0,0 +1,6 @@
+/*
+    Utilize a estrutura de dados adequada e implemente um programa que converta
+    um número decimal em um número binário.
+
+    Data de entrega: 26/05, até 11h20, valendo nota de participação
+*/
---
+++ ex-estruturas02.mjs
@@ -0,0 +1,5 @@
+/*
+    Implemente um programa verificador de palíndromos utilizando um deque.
+
+    Data de entrega: 26/05, até 11h20, valendo nota de participação.
+*/
